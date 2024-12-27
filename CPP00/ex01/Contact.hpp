b975b7e68a793716ed230da7a81848bce4eecc59
flmarsou <marsouinflorent12@gmail.com>
2024-12-27 15:56:38 +0100

    Norm CPP

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -18,11 +18,11 @@
 class Contact
 {
 	private:
-		std::string	firstName;
-		std::string	lastName;
-		std::string	nickname;
-		std::string	phoneNumber;
-		std::string	darkestSecret;
+		std::string	_first_name;
+		std::string	_last_name;
+		std::string	_nickname;
+		std::string	_phone_number;
+		std::string	_darkest_secret;
 };
 
 #endif
