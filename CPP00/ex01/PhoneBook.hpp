958e5f3765bf78bcda25b93847a3512bb6509a4c
flmarsou <marsouinflorent12@gmail.com>
2024-12-31 15:09:51 +0100

    Added Constructor, Methods, and variables

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 12:56:01 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/27 15:32:22 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/31 15:09:32 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <iostream>
 
+# include "Contact.hpp"
+
 # define ITALIC	"\e[3m"
 # define GREEN	"\e[32m"
 # define RESET	"\e[0m"
@@ -22,8 +24,15 @@
 class PhoneBook
 {
 	public:
+		// Constructor
+		PhoneBook();
+		// Methods
 		bool	addContact();
 		bool	searchContact();
+	private:
+		Contact			_contacts[8];
+		unsigned int	_index;
+		unsigned int	_nbr_of_contacts;
 };
 
 #endif
