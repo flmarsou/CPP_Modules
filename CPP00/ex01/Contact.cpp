5095c8f9e643fd165b293c25215879ee1e62e552
flmarsou <marsouinflorent12@gmail.com>
2025-01-02 09:43:44 +0100

    Prettier display

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 11:02:45 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/31 13:16:45 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/02 09:16:58 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,18 @@ void	Contact::setDarkestSecret(std::string darkestSecret) { this->_darkest_secre
 static void	truncSearch(std::string str)
 {
 	if (str.length() > 10)
-		std::cout << std::setw(9) << str.substr(0, 9) << "." << "|";
+		std::cout << std::setw(9) << str.substr(0, 9) << "." << "║";
 	else
-		std::cout << std::setw(10) << str  << "|";
+		std::cout << std::setw(10) << str  << "║";
 }
 
 void	Contact::printSearch(unsigned int index)
 {
 	// Index
-	std::cout << "|" << std::setw(10) << index << "|";
+	std::cout << "║" << std::setw(10) << index << "║";
 	truncSearch(this->_first_name);
 	truncSearch(this->_last_name);
 	truncSearch(this->_nickname);
-	std::cout << "\n";
 }
 
 void	Contact::printContact()
