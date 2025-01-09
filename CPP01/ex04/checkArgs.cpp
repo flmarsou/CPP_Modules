ad0f6ea99a5335b90a4c7db7f918636c3be7a07d
flmarsou <marsouinflorent12@gmail.com>
2025-01-09 11:14:49 +0100

    Define Prints

--- CPP01/ex04/checkArgs.cpp
+++ CPP01/ex04/checkArgs.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:15:18 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/08 14:38:55 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/09 11:13:07 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ bool	checkArgc(int argc, const char **argv)
 	switch (argc)
 	{
 		case 1:
-			std::cerr << RED "[x] - Error:" RESET " Missing arguments!" << std::endl;
-			std::cerr << BLUE "[i] - Usage:" RESET " ./sed <filename> <s1> <s2>" << std::endl;
+			std::cerr << ERROR "Missing arguments!" << std::endl;
+			std::cerr << USAGE "./sed <filename> <s1> <s2>" << std::endl;
 			return (false);
 		case 2:
-			std::cerr << RED "[x] - Error:" RESET " Missing s1 and s2!" << std::endl;
-			std::cerr << BLUE "[i] - Usage:" RESET " ./sed " << argv[1] << " <s1> <s2>" << std::endl;
+			std::cerr << ERROR "Missing s1 and s2!" << std::endl;
+			std::cerr << USAGE "./sed " << argv[1] << " <s1> <s2>" << std::endl;
 			return (false);
 		case 3:
-			std::cerr << RED "[x] - Error:" RESET " Missing s2!" << std::endl;
-			std::cerr << BLUE "[i] - Usage:" RESET " ./sed " << argv[1] << " " << argv[2] << " <s2>" << std::endl;
+			std::cerr << ERROR "Missing s2!" << std::endl;
+			std::cerr << USAGE "./sed " << argv[1] << " " << argv[2] << " <s2>" << std::endl;
 			return (false);
 		default:
 			if (argc > 4)
-				std::cerr << PURPLE "[!] - Warning:" RESET " Too many arguments!" << std::endl;
-			break ;
+				std::cerr << WARNING "Too many arguments!" << std::endl;
+			return (true);
 	}
-	std::cout << GREEN << "[v] - Success:" RESET " Argument count valid!" << std::endl;
+	std::cout << SUCCESS "Argument count valid!" << std::endl;
 	return (true);
 }
 
@@ -41,19 +41,19 @@ bool	checkArgv(const char **argv)
 {
 	if (argv[1][0] == '\0')
 	{
-		std::cerr << RED "[x] - Error:" RESET " Filename is empty!" << std::endl;
+		std::cerr << ERROR "Filename is empty!" << std::endl;
 		return (false);
 	}
 	else if (argv[2][0] == '\0')
 	{
-		std::cerr << RED "[x] - Error:" RESET " s1 is empty!" << std::endl;
+		std::cerr << ERROR "s1 is empty!" << std::endl;
 		return (false);
 	}
 	else if (argv[3][0] == '\0')
 	{
-		std::cerr << RED "[x] - Error:" RESET " s2 is empty!" << std::endl;
+		std::cerr << ERROR "s2 is empty!" << std::endl;
 		return (false);
 	}
-	std::cout << GREEN << "[v] - Success:" RESET " Argument values valid!" << std::endl;
+	std::cout << SUCCESS "Argument values valid!" << std::endl;
 	return (true);
 }
