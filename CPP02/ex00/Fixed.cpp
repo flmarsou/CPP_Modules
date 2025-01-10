b2d9513a77649e6c82e9ca1a0c5f9b7657d9f626
flmarsou <marsouinflorent12@gmail.com>
2025-01-10 10:18:23 +0100

    Updated CPP02 Ex00

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,8 +6,42 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:48:07 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/09 14:48:13 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/10 10:16:19 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed() : _number(0)
+{
+	std::cout << GRAY "Default constructor called" RESET << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << GRAY "Copy constructor called" RESET << std::endl;
+	this->_number = fixed._number;
+}
+
+Fixed	&Fixed::operator=(const Fixed &other)
+{
+	std::cout << GRAY "Copy assignment operator called" RESET << std::endl;
+	this->_number = other._number;
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << GRAY "Destructor called" RESET << std::endl;
+}
+
+int		Fixed::getRawBits()
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_number);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	this->_number = raw;
+}
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:42:01 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/09 14:47:54 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/10 09:32:00 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,24 @@
 
 # include <iostream>
 
+# define GRAY		"\e[30m"
+# define RED		"\e[31m"
+# define GREEN		"\e[32m"
+# define BLUE		"\e[36m"
+# define RESET		"\e[0m"
+
 class Fixed
 {
 	public:
-		Fixed();
-		//
-		//
-		~Fixed();
+		Fixed();								// Constructor
+		Fixed(const Fixed &fixed);				// Copy Constructor
+		Fixed 	&operator=(const Fixed &Fixed);	// Assignment Operator
+		~Fixed();								// Destructor
 		int		getRawBits();
 		void	setRawBits(const int raw);
 	private:
 		int					_number;
-		static const int	_number_of_bytes;
+		static const int	_number_of_bytes = 8;
 };
 
 #endif
