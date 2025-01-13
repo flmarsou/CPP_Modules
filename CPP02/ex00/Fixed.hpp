0cee2b4e803bbd00c120afb1b70f7c03bf9f9696
flmarsou <marsouinflorent12@gmail.com>
2025-01-13 08:54:43 +0100

    Norm

--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 14:42:01 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/10 09:32:00 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/10 11:58:20 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,6 @@
 # include <iostream>
 
 # define GRAY		"\e[30m"
-# define RED		"\e[31m"
-# define GREEN		"\e[32m"
-# define BLUE		"\e[36m"
 # define RESET		"\e[0m"
 
 class Fixed
@@ -26,7 +23,7 @@ class Fixed
 	public:
 		Fixed();								// Constructor
 		Fixed(const Fixed &fixed);				// Copy Constructor
-		Fixed 	&operator=(const Fixed &Fixed);	// Assignment Operator
+		Fixed 	&operator=(const Fixed &other);	// Assignment Operator
 		~Fixed();								// Destructor
 		int		getRawBits();
 		void	setRawBits(const int raw);
