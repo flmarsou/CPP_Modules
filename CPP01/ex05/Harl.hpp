4f3b59bdcbd456e893b8058936a5e3e83ef7b605
flmarsou <marsouinflorent12@gmail.com>
2025-01-21 10:35:16 +0100

    Norm

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 11:56:06 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/09 14:10:30 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/21 10:35:06 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,15 @@
 class Harl
 {
 	public:
+		// Constructors
 		Harl();
+
+		// Destructors
 		~Harl();
+
+		// Methods
 		void	complain(std::string level);
+
 	private:
 		void	debug();
 		void	info();
