6f8b975c0f9679d53b35a0e65d1c82d6b11d8e26
flmarsou <marsouinflorent12@gmail.com>
2025-01-22 10:59:29 +0100

    Update Fixed.cpp

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -10,3 +10,74 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+// ========================================================================== //
+//   Constructors                                                             //
+// ========================================================================== //
+
+Fixed::Fixed() : _number(0)
+{
+	std::cout << GRAY "Default constructor called" RESET << std::endl;
+}
+
+Fixed::Fixed(const int nbr)
+{
+	std::cout << GRAY "Int constructor called" RESET << std::endl;
+	this->_number = nbr;
+}
+
+Fixed::Fixed(const float flt)
+{
+	std::cout << GRAY "Float constructor called" RESET << std::endl;
+	this->_number = roundf(flt * (1 << this->_number_bits_fraction));
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << GRAY "Copy constructor called" RESET << std::endl;
+	this->_number = copy._number;
+}
+
+// ========================================================================== //
+//   Operators                                                                //
+// ========================================================================== //
+
+Fixed			&Fixed::operator=(const Fixed &other)
+{
+	std::cout << GRAY "Copy assignment operator called" RESET << std::endl;
+	this->_number = other._number;
+	return (*this);
+}
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
+
+// ========================================================================== //
+//   Destructors                                                              //
+// ========================================================================== //
+
+Fixed::~Fixed()
+{
+	std::cout << GRAY "Destructor called" RESET << std::endl;
+}
+
+// ========================================================================== //
+//   Methods & Functions                                                      //
+// ========================================================================== //
+
+
+// Converts the fixed-point value to a floating-point value.
+float	Fixed::toFloat() const
+{
+	
+}
+
+// Converts the fixed-point value to an integer value.
+int		Fixed::toInt() const
+{
+	
+}
