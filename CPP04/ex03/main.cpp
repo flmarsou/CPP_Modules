3ccb743c30097086b653446533bed5793da42ab5
flmarsou <marsouinflorent12@gmail.com>
2025-03-28 13:17:45 +0100

    Subject Main

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -6,36 +6,36 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 14:49:02 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/03/19 18:01:21 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/03/28 13:14:33 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "IMateriaSource.hpp"
 #include "ICharacter.hpp"
-
 #include "AMateria.hpp"
-
 #include "MateriaSource.hpp"
 #include "Character.hpp"
 #include "Ice.hpp"
-//#include "Cure.hpp"
+#include "Cure.hpp"
 
-int main()
+int	main()
 {
-	IMateriaSource	*src = new MateriaSource();
+	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
-	//src->learnMateria(new Cure());
+	src->learnMateria(new Cure());
 
-	ICharacter		*me = new Character("me");
-	AMateria		*tmp;
+	ICharacter* me = new Character("player");
+	ICharacter* bob = new Character("enemy");
+
+	AMateria* tmp;
 	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	//tmp = src->createMateria("cure");
-	//me->equip(tmp);
 
-	ICharacter		*bob = new Character("bob");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
 	me->use(0, *bob);
-	//me->use(1, *bob);
+	me->use(1, *bob);
 
 	delete bob;
 	delete me;
