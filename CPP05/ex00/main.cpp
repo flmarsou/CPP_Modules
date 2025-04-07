1cdb7188969c9f850a35ab6b6fbb5c377fbdff29
flmarsou <marsouinflorent12@gmail.com>
2025-04-07 15:36:55 +0200

    Better tests

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,54 +6,55 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 16:19:42 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/03 15:19:04 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/07 12:26:44 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int	main()
+void	tryModifyGrade(const std::string name, int grade, char modifyGrade)
 {
 	try
 	{
-		Bureaucrat	toto("Toto", 42);
-		std::cout << toto << std::endl;
-		Bureaucrat	tata("Tata", 1);
-		std::cout << tata << std::endl;
-		Bureaucrat	titi("Titi", 150);
-		std::cout << titi << std::endl;
+		Bureaucrat	test(name, grade);
+		if (modifyGrade == '+')
+			test.incrementGrade();
+		else if (modifyGrade == '-')
+			test.decrementGrade();
+		std::cout << test << std::endl;
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+}
 
+void	tryBureaucrat(const std::string name, int grade)
+{
 	try
 	{
-		Bureaucrat	wrongToto("WrongToto", -42);
+		Bureaucrat	test(name, grade);
+		std::cout << test << std::endl;
 	}
-	catch(const std::exception &e)
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
+}
 
-	try
-	{
-		Bureaucrat	wrongTata("WrongTata", 0);
-	}
-	catch(const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+int	main()
+{
+	tryBureaucrat("Toto", 42);
+	tryBureaucrat("Tata", 1);
+	tryBureaucrat("Titi", 150);
+	tryBureaucrat("WrongToto", -42);
+	tryBureaucrat("WrongTata", 0);
+	tryBureaucrat("WrongTiti", 151);
 
-	try
-	{
-		Bureaucrat	wrongTiti("WrongTiti", 151);
-	}
-	catch(const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	tryModifyGrade("Decrease", 42, '-');
+	tryModifyGrade("Increase", 42, '+');
+	tryModifyGrade("WrongDecrease", 150, '-');
+	tryModifyGrade("WrongIncrease", 1, '+');
 
 	return (0);
 }
