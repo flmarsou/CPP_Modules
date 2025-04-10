fedfa0a790bc83fa398e72a9f6ca60525a3b020c
flmarsou <marsouinflorent12@gmail.com>
2025-04-10 15:43:40 +0200

    Added misc, float, and double conversion

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:28:51 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/09 16:34:13 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:02:11 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,48 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &other)
 //   Methods & Functions                                                      //
 // ========================================================================== //
 
+static bool	isMisc(const std::string &input)
+{
+	if (input == "+inf" || input == "+inff")
+	{
+		std::cout << CHAR "Impossible" << std::endl;
+		std::cout << INT "Impossible" << std::endl;
+		std::cout << FLOAT "+inff" << std::endl;
+		std::cout << DOUBLE "+inf" << std::endl;
+		return (true);
+	}
+	else if (input == "-inf" || input == "-inff")
+	{
+		std::cout << CHAR "Impossible" << std::endl;
+		std::cout << INT "Impossible" << std::endl;
+		std::cout << FLOAT "-inff" << std::endl;
+		std::cout << DOUBLE "-inf" << std::endl;
+		return (true);
+	}
+	else if (input == "nan" || input == "nanf")
+	{
+		std::cout << CHAR "Impossible" << std::endl;
+		std::cout << INT "Impossible" << std::endl;
+		std::cout << FLOAT "nanf" << std::endl;
+		std::cout << DOUBLE "nan" << std::endl;
+		return (true);
+	}
+	return (false);
+}
+
+
 void	ScalarConverter::convert(const std::string &input)
 {
-	if (isChar(input))
+	if (isMisc(input))
+		return ;
+	else if (isChar(input))
 		castToChar(input);
 	else if (isInt(input))
 		castToInt(input);
+	else if (isFloat(input))
+		castToFloat(input);
+	else if (isDouble(input))
+		castToDouble(input);
 	else
 		std::cout << ERROR "Wrong Argument!" << std::endl;
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:38:16 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/09 16:50:56 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/10 14:59:56 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 # include <iostream>
 # include <iomanip>
 # include <cstdlib>
+# include <cstring>
+# include <cmath>
+# include <limits>
 
 # define ERROR	"\e[31m[ ERROR ] - \e[0m"
 
@@ -23,10 +26,14 @@
 # define FLOAT	"\e[32m[ FLOAT  ]\e[0m -> "
 # define DOUBLE	"\e[32m[ DOUBLE ]\e[0m -> "
 
-# define CHAR_MAX	127
-# define CHAR_MIN	-128
-# define INT_MAX	2147483647
-# define INT_MIN	-2147483648
+# define MAX_CHAR	std::numeric_limits<char>::max()	// 127
+# define MIN_CHAR	std::numeric_limits<char>::min()	// -128
+# define MAX_INT	std::numeric_limits<int>::max()		// 2147483647
+# define MIN_INT	std::numeric_limits<int>::min()		// -2147483648
+# define MAX_FLOAT	std::numeric_limits<float>::max()	// 3.40282347e+38F
+# define MIN_FLOAT	std::numeric_limits<float>::min()	// 1.17549435e-38F
+# define MAX_DOUBLE	std::numeric_limits<double>::max()	// 1.7976931348623157e+308
+# define MIN_DOUBLE	std::numeric_limits<double>::min()	// 2.2250738585072014e-308
 
 class ScalarConverter
 {
@@ -45,5 +52,12 @@ class ScalarConverter
 
 bool	isChar(const std::string &input);
 void	castToChar(const std::string &input);
+
 bool	isInt(const std::string &input);
 void	castToInt(const std::string &input);
+
+bool	isFloat(const std::string &input);
+void	castToFloat(const std::string &input);
+
+bool	isDouble(const std::string &input);
+void	castToDouble(const std::string &input);
