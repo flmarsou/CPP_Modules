a0003f07ae5b16ce1052a87d302be5202f6cab8e
flmarsou <marsouinflorent12@gmail.com>
2025-04-11 11:58:51 +0200

    Added reinterpret casts

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:54:20 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/10 15:55:35 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/11 10:30:26 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ Serializer::Serializer()
 
 Serializer::Serializer(const Serializer &other)
 {
-
+	(void)other;
 }
 
 Serializer::~Serializer()
@@ -37,9 +37,20 @@ Serializer::~Serializer()
 
 Serializer	&Serializer::operator=(const Serializer &other)
 {
-
+	(void)other;
+	return (*this);
 }
 
 // ========================================================================== //
 //   Methods & Functions                                                      //
 // ========================================================================== //
+
+uintptr_t	Serializer::serialize(t_data *ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+t_data		*Serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<t_data *>(raw));
+}
