898a19ef25b77d575564bb7c4edc180fd2f9660e
flmarsou <marsouinflorent12@gmail.com>
2025-04-21 14:06:07 +0200

    Added defines and includes

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,19 +6,21 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:34:27 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/18 15:35:31 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/21 14:03:22 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-# include <iostream>
-# include <map>
 # include <fstream>
+# include <iostream>
 # include <cstdlib>
 # include <cmath>
+# include <map>
 
 # define ERROR	"\e[31m[ ERROR ] - \e[0m"
+# define KO		"\e[31m[ KO ] \e[0m"
+# define OK		"\e[32m[ OK ] \e[0m"
 
 class BitcoinExchange
 {
@@ -33,6 +35,5 @@ class BitcoinExchange
 		void	loadWallet(const char *filePath);
 
 	private:
-		std::map<std::string, float>	_db;
-		std::map<std::string, float>	_wallet;
+		std::map<std::string, double>	_db;
 };
