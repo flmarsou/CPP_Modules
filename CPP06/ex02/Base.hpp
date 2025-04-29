5d5ab8d1cbe11f92cb8293ee9f4cbdbf348f9154
flmarsou <marsouinflorent12@gmail.com>
2025-04-29 10:44:51 +0200

    Added wrong class

--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 12:00:18 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/11 13:04:38 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/29 10:42:15 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,4 @@ class Base
 class A : public Base {};
 class B : public Base {};
 class C : public Base {};
+class D : public Base {};
