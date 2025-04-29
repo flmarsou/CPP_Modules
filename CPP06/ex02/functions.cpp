62c81c1f27d6141ff645a048f9bb80da9cf3f905
flmarsou <marsouinflorent12@gmail.com>
2025-04-29 10:44:38 +0200

    Fixed pointer to address with try/catch

--- CPP06/ex02/functions.cpp
+++ CPP06/ex02/functions.cpp
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 12:11:51 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/04/11 13:04:39 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/29 10:44:00 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 Base	*generate()
 {
-	int	random = rand() % 3;
+	int	random = rand() % 4;
 	if (random == 1)
 		return (new A);
 	else if (random == 2)
 		return (new B);
-	else
+	else if (random == 3)
 		return (new C);
+	else
+		return (new D);
 }
 
 void	identify(Base *p)
 {
-	if (dynamic_cast<A*>(p))
+	if (dynamic_cast<A *>(p))
 		std::cout << "Identified with pointer, instance of A class!" << std::endl;
-	else if (dynamic_cast<B*>(p))
+	else if (dynamic_cast<B *>(p))
 		std::cout << "Identified with pointer, instance of B class!" << std::endl;
-	else if (dynamic_cast<C*>(p))
+	else if (dynamic_cast<C *>(p))
 		std::cout << "Identified with pointer, instance of C class!" << std::endl;
 	else
 		std::cout << "Identified with pointer, not an instance of A, B, or C!" << std::endl;
@@ -37,12 +39,29 @@ void	identify(Base *p)
 
 void	identify(Base &p)
 {
-	if (dynamic_cast<A*>(&p))
+	try
+	{
+		(void)dynamic_cast<A &>(p);
 		std::cout << "Identified with address, instance of A class!" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "Identified with address, instance of B class!" << std::endl;
-	else if (dynamic_cast<C*>(&p))
-		std::cout << "Identified with address, instance of C class!" << std::endl;
-	else
-		std::cout << "Identified with address, not an instance of A, B, or C!" << std::endl;
+	}
+	catch (...)
+	{
+		try
+		{
+			(void)dynamic_cast<B &>(p);
+			std::cout << "Identified with address, instance of B class!" << std::endl;
+		}
+		catch (...)
+		{
+			try
+			{
+				(void)dynamic_cast<C &>(p);
+				std::cout << "Identified with address, instance of C class!" << std::endl;
+			}
+			catch (...)
+			{
+				std::cout << "Identified with address, not an instance of A, B, or C!" << std::endl;
+			}
+		}
+	}
 }
